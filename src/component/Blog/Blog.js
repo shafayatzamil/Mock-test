c12462485836eaf2d20a700685fa17e150d7fdcd
shafayat zamil <zamilkhan111.za@gmail.com>
2022-10-11 01:09:45 +0600

    blogpost question 2

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -5,6 +5,8 @@ const Blog = () => {
         <div>
             <h2> what is the purpose of react router?</h2>
             <p>React Router plays an important role to display multiple views in a single page application. Without React Router, it is not possible to display multiple views in React applications. Most of the social media websites like Facebook, Instagram uses React Router for rendering multiple views.</p>
+            <h2>what does context API work?</h2>
+            <p>The Context API can be used to share data with multiple components, without having to pass data through props manually.</p>
             
         </div>
     );

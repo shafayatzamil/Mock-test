9de84c6e07a2dd5e91fcc7f9129212161595e8c0
shafayat zamil <zamilkhan111.za@gmail.com>
2022-10-11 01:05:12 +0600

    blog post question 1

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            this is blog post
+            <h2> what is the purpose of react router?</h2>
+            <p>React Router plays an important role to display multiple views in a single page application. Without React Router, it is not possible to display multiple views in React applications. Most of the social media websites like Facebook, Instagram uses React Router for rendering multiple views.</p>
+            
         </div>
     );
 };
